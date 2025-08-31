f6d0b10423ad58431144042175c5d47e81aabea6
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-31 15:29:08 +0300

    added morgan logger

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import recipesRouter from "./routes/recipesRouter";
 import { BaseError } from "./errors";
+import morgan from "morgan";
 
 const server = express();
 const port = 3030;
+const logFormat = ':date[iso] :method :status';
 
 server.use(express.json());
+server.use(morgan(logFormat));
 
 server.use("/api/recipes",recipesRouter);
 
@@ -17,7 +20,8 @@ server.listen(port, () => {
 server.use((err:BaseError, req:Request, res:Response, next:NextFunction) => {
   console.error(err);
   res.status(err.status || 500).json({
-    success: false,
+    error: true,
     message: err.message || "Server Error",
+    statusCode:err.status
   });
 });

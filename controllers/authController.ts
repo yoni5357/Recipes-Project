767ebbd82ca2b305597ea8c17e35cc339c4c311a
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-04 15:40:53 +0300

    added jwt to authController

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 import usersModel from "../models/usersModel";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 async function addUser(req:Request, res:Response){
     const body = req.body;
     const user = await usersModel.addUser(body);
+    const token = jwt.sign(user,process.env.JWT_SECRET);
     res.status(201);
-    res.send(user);
+    res.send({...user,token});
 }
 
 export default {addUser};
